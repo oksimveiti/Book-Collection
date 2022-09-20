272b1be11909b3dd8184477f0f983fc784c11922
Semih Cetin <semihcettin@gmail.com>
2022-09-20 21:27:21 +0300

    Initial Commit

---
+++ Book Collection/Book_CollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Book_CollectionApp.swift
+//  Book Collection
+//
+//  Created by Semih Cetin on 20.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct Book_CollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
